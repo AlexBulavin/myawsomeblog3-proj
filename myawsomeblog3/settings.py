dfb30178426e77d7c6835edba58990f846ec0f43
AlexBulavin <bulavin1970@icloud.com>
2021-06-06 18:52:43 +0300

    Add postgres DB to project

--- myawsomeblog3/settings.py
+++ myawsomeblog3/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'myawsomeblog3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'myawsomeblog3db',
+      'USER': 'postgres',
+      'PASSWORD': 'my123password86-193pg',
+      'HOST': 'localhost',
+      'PORT': '5432',
     }
 }
 
