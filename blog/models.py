598609d5454eb750bd84f19d0b7177f79d1c5d1f
AlexBulavin <bulavin1970@icloud.com>
2021-06-08 08:53:37 +0300

    Add single post page but unfinished

--- blog/models.py
+++ blog/models.py
@@ -7,4 +7,8 @@ class Post(models.Model):
     post_time = models.DateTimeField()
     post_image = models.ImageField(upload_to = 'post_images/')
     post_text = models.CharField(max_length = 500)
+
+    def get_summary(self):
+        return self.text[:70]
     #print('Class Post called')
+
